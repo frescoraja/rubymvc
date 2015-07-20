c2b70c74982aadb071f3067d7698c3a4d163dc61
David James Carter <fresco.raja@gmail.com>
2015-07-20 01:04:55 -0700

    route helpers added

--- lib/route.rb
+++ lib/route.rb
@@ -1,4 +1,6 @@
+require_relative "./route_helpers"
 class Route
+  include RouteHelpers
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
@@ -6,6 +8,48 @@ class Route
     @http_method = http_method
     @controller_class = controller_class
     @action_name = action_name
+    add_route_helpers
+  end
+
+  def add_route_helpers
+    case action_name
+      when :create, :index
+        name = "#{class_name_plural}"
+        add_path_method(name, "/#{name}")
+      when :show, :update, :destroy
+        name = "#{class_name_singular}"
+        add_path_method(name, "/#{class_name_plural}/:id")
+      when :edit
+        name = "edit_#{class_name_singular}"
+        add_path_method(name, "/#{class_name_plural}/:id/edit")
+      when :new
+        name = "new_#{class_name_singular}"
+        add_path_method(name, "/#{class_name_plural}/new")
+  end
+
+  def class_name
+    controller_class.to_s.underscore.gsub("_controller", "")
+  end
+
+  def class_name_singular
+    class_name.singularize
+  end
+
+  def class_name_plural
+    class_name.pluralize
+  end
+
+  def add_path_method(name, path)
+    path_name = "#{name}_path"
+    puts "#{path_name} #=> #{path}"
+
+    RouteHelpers.send(:define_method, path_name) do |*args|
+      id = args.first.to_s
+      if path.include?(":id") && !id.nil?
+        path.gsub!(":id", id)
+      end
+      path
+    end
   end
 
   # checks if pattern matches path and method matches request method
